66b25b22ec8cc1c907e3fb64ec6e748b675c3a96
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-06-18 21:03:22 +0200

    fix: error

--- photopickerTests/photopickerTests.swift
+++ photopickerTests/photopickerTests.swift
@@ -15,7 +15,7 @@ final class photopickerTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() throws {
