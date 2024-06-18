84103431b761008c907b90ba1df0246cd68cd4af
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2024-06-18 21:04:20 +0200

    edit: image styles

--- photopickerTests/photopickerTests.swift
+++ photopickerTests/photopickerTests.swift
@@ -23,9 +23,7 @@ final class photopickerTests: XCTestCase {
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
